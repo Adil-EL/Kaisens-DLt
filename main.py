443083c57c917495c4163cca0ee6173a463fafae
adil-EL <adil.elhakouni@etu.emse.fr>
2021-10-05 16:28:00 +0200

    Add_invest

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from flask import Flask
+import pandas as pandas
+
 
 app = Flask(__name__)
 
+def alimente_caisse(date,amount,type):
+    if type == 'PRET':
+        pass
+    elif type == 'INVEST':
+        pass
+    elif type == 'SALE':
+        pass
+    else :
+        print('PLease specify the origin of the input !')
+
+def add_invest(**kwargs):
+    pass
